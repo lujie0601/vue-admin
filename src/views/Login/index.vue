0c3ab5edd9c84ff9146f55a3aecf70dec1139ba4
unknown <lujie0601@gmail.com>
2020-08-27 11:35:43 +0800

    构建项目、配置全局样式、路由

---
+++ src/views/Login/index.vue
@@ -0,0 +1,30 @@
+<template>
+    <div id="login">
+sd
+
+        <div class="login-wrap">
+
+        </div>
+    </div>
+</template>
+<script>
+export default {
+    name:'login',
+    data(){
+        return{
+
+        }
+    },
+    created(){
+        console.log("created")
+    },
+    mounted(){
+        console.log("mounted")
+    },
+    methods:{},
+    props:{}
+}
+</script>
+<style lang="scss"  scoped>
+
+</style>
