7f7bc6b889ea1f9675c346c5c2a17d8495863052
unknown <lujie0601@gmail.com>
2020-08-28 15:34:09 +0800

    登录页面静态制作、input的数据校验

--- src/main.js
+++ src/main.js
@@ -1,13 +1,17 @@
-import { createApp } from "vue";
+import Vue from "vue";
 import App from "./App.vue";
 import router from "./router";
+import ElementUI from 'element-ui';
+import 'element-ui/lib/theme-chalk/index.css';
 import store from "./store";
 
+Vue.config.productionTip = false;
 
 
+Vue.use(ElementUI);
 
-
-createApp(App)
-  .use(store)
-  .use(router)
-  .mount("#app");
+new Vue({
+  router,
+  store,
+  render: h => h(App)
+}).$mount("#app");
--- src/router/index.js
+++ src/router/index.js
@@ -1,12 +1,12 @@
-import { createRouter, createWebHashHistory } from "vue-router";
-import Home from "../views/Home.vue";
+import Vue from "vue";
+import VueRouter from "vue-router";
 import Login from "../views/Login/index.vue"
+
+Vue.use(VueRouter);
 
 const routes = [
   {
     path: "/",
-/*     name: "Home",
-    component: Home */
     redirect:"login"
   },
   {
@@ -20,15 +20,12 @@ const routes = [
     // route level code-splitting
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
-    component: function()
-      {
-        return import(/* webpackChunkName: "about" */ "../views/About.vue");
-      }
+    component: () =>
+      import(/* webpackChunkName: "about" */ "../views/About.vue")
   }
 ];
 
-const router = createRouter({
-  history: createWebHashHistory(),
+const router = new VueRouter({
   routes
 });
 
